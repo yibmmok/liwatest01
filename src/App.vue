a9f90b120a41a14edf962d7a72b885ef084a5803
yibmmok <liwa0000@gmail.com>
2021-10-13 21:27:48 +0800

    20211013@2127

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,45 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- <div class="container"> -->
+    <img id="img1" :src="data.src" />
+  <!-- </div> -->
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
+  data() {
+    return {
+      data: {
+        src: {}
+      }
+    }
+  },
+  beforeMount(){
+    this.getPhoto();
+  },
   components: {
-    HelloWorld
+
+  },
+  methods: {
+    async getPhoto() {
+      let url = 'https://images.unsplash.com/photo-1513313778780-9ae4807465f0?auto=format&fit=crop&w=634&q=80';
+      // const res = await fetch('https://api.agify.io/?name=michael');
+      const request = new Request(
+        url, {
+          method: "GET",
+          mode: "cors",
+          cache: "default"
+        }
+      );
+      const res = await fetch(request);
+      const imageBlob = await res.blob();
+      let img = document.createElement('IMG');
+      document.querySelector('#img1').appendChild(img);
+      img.src = URL.createObjectURL(imageBlob);
+      this.data.src = img.src;
+    }
   }
 }
 </script>
@@ -22,5 +52,10 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.container {
+  width:100%;
+  height:100vh;
 }
 </style>
